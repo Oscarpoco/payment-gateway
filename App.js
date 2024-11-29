6a258a81816db6ad8a1aff0bd8ada3f32c6ce71a
Oscar Poco <123397536+Oscarpoco@users.noreply.github.com>
2024-11-29 15:20:47 +0200

    I have implemented payment gateway

--- App.js
+++ App.js
@@ -1,55 +1,116 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
+import Pay from './payment.js';
 
 export default function App() {
-
   const [onPay, setOnPay] = useState(false);
   const [lightMode, setLightMode] = useState(true);
+
   return (
-    <View style={styles.container}>
-      
-      {onPay ? 
-      
-      <View>
-
-      </View>
-
-      :
-
-      <View style={styles.ItemParent}>
-
-        {/* IMAGE */}
-          <View style={[styles.imageContainer, {backgroundColor: lightMode ? '#f0f0f0' : '#333'}]}>
-            <Image source={require('./assets/playstation.jpg')} style={styles.image} />
+    <View style={[styles.container, { backgroundColor: lightMode ? '#fff' : '#000' }]}>
+      {onPay ? (
+        <Pay onClose={() => setOnPay(false)} />
+      ) : (
+        <View style={styles.ItemParent}>
+          {/* IMAGE */}
+          <View 
+            style={[
+              styles.imageContainer, 
+              { backgroundColor: lightMode ? '#f0f0f0' : '#444' }
+            ]}
+          >
+            <Image 
+              source={require('./assets/playstation.jpg')} 
+              style={styles.image} 
+              resizeMode="contain"
+            />
           </View>
-        {/* ENDS */}
 
-        {/* ITEM NAME */}
-        <View style={styles.itemNameContainer}>
-          <Text style={[styles.text, { color: lightMode ? '#000' : '#fff' }]}>
-            Playstation 5
-          </Text>
-          <Text style={[styles.text, { color: lightMode ? '#000' : '#fff' }]}>
-            R7000
-          </Text>
+          {/* ITEM NAME */}
+          <View style={styles.itemNameContainer}>
+            <Text style={[styles.text, { color: lightMode ? '#000' : '#fff' }, {letterSpacing: 2}]}>
+              Playstation 5
+            </Text>
+            <Text style={[styles.text, { color: lightMode ? '#ab173f' : '#ab173f' }, { backgroundColor: lightMode ? '#1AEB84' : '#1AEB84' }, {paddingHorizontal: 15, paddingVertical: 3, borderRadius: 10}]}>
+              R7000
+            </Text>
+          </View>
+
+          {/* DESCRIPTION */}
+          <View style={styles.descriptionContainer}>
+
+            <Text 
+              style={[
+                styles.descriptionText, 
+                { 
+                  color: lightMode ? '#000' : '#fff',
+                  textAlign: 'center'
+                }
+              ]}
+            >
+              Experience gaming like never before with the PlayStation 5 - 
+              cutting-edge performance and immersive next-gen gaming.
+            </Text>
+          </View>
+
+          <View style={styles.recommendContainerWrapper}>
+          <Text style={[styles.text, { color: lightMode ? '#000' : '#fff' }, {paddingHorizontal: 5, fontWeight: 600}]}>Choose the item to view</Text>
+            <View style={styles.recommendContainer}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <TouchableOpacity style={[styles.recommendItems, { backgroundColor: lightMode ? '#f0f0f0' : '#444' }]}>
+                <Image 
+                source={require('./assets/apple.jpg')} 
+                style={styles.imageRec} 
+                resizeMode="contain"
+                />
+              </TouchableOpacity>
+              
+              <TouchableOpacity style={[styles.recommendItems, { backgroundColor: lightMode ? '#f0f0f0' : '#444' }]}>
+                <Image 
+                source={require('./assets/pc.jpg')} 
+                style={styles.imageRec} 
+                resizeMode="contain"
+                />
+              </TouchableOpacity>
+
+              <TouchableOpacity style={[styles.recommendItems, { backgroundColor: lightMode ? '#f0f0f0' : '#333' }]}>
+                <Image 
+                source={require('./assets/headphone.jpg')} 
+                style={styles.imageRec} 
+                resizeMode="contain"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.recommendItems, { backgroundColor: lightMode ? '#f0f0f0' : '#333' }]}>
+                <Image 
+                source={require('./assets/Huawei.jpg')} 
+                style={styles.imageRec} 
+                resizeMode="contain"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.recommendItems, { backgroundColor: lightMode ? '#f0f0f0' : '#333' }]}>
+                <Image 
+                source={require('./assets/monitor.jpg')} 
+                style={styles.imageRec} 
+                resizeMode="contain"
+                />
+              </TouchableOpacity>
+              </ScrollView>
+            </View>
+          </View>
+
+          {/* Checkout Button */}
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => setOnPay(true)}
+            >
+              <Text style={styles.buttonText}>Checkout</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        {/* ENDS */}
-
-        {/* DESCRIPTION */}
-        <Text style={[styles.text, { color: lightMode ? '#000' : '#fff' }]}>
-        The syntax you provided for the style prop in the component is incorrect. The issue lies in the way  trying to conditionally set the color property inside the array.
-        </Text>
-        {/* ENDS */}
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={() => setOnPay(true)}>
-            <Text style={styles.buttonText}>Checkout</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-    }
-      <StatusBar style="auto" />
+      )}
+      <StatusBar style= {lightMode ? 'dark' : 'light'} />
     </View>
   );
 }
@@ -59,8 +120,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
-
+    paddingHorizontal: 20,
+    paddingVertical: 30,
   },
 
   ItemParent: {
@@ -68,70 +129,115 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'column',
-    gap: 10,
-    width: '100%'
+    gap: 5,
+    width: '100%',
   },
 
   // BUTTON
-  button: 
-  {
+  button: {
     width: '100%',
     height: 50,
-    backgroundColor: '#007bff',
+    backgroundColor: '#1AEB84',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10,
-    marginVertical: 10
+    paddingVertical: 10,
   },
 
-  buttonText: 
-  {
+  buttonText: {
     fontSize: 18,
     color: '#fff',
     fontWeight: 'bold',
   },
 
   // IMAGE
-  imageContainer: 
-  {
-    padding: 20,
+  imageContainer: {
+    padding: 10,
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 10
+    borderRadius: 10,
   },
 
-  image: 
-  {
+  image: {
     width: 300,
     height: 250,
     resizeMode: 'contain',
-
   },
 
-  itemNameContainer:
-  {
+  itemNameContainer: {
     paddingVertical: 10,
+    paddingHorizontal: 5,
     marginVertical: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    width: '100%'
+    width: '100%',
   },
 
-  text:
-  {
+  text: {
     fontSize: 18,
     fontWeight: 600,
   },
 
-  buttonContainer:
-  {
+  buttonContainer: {
     width: '100%',
     marginVertical: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-  }
+  },
 
+  recommendContainerWrapper:
+  {
+    width: '100%',
+    marginVertical: 10,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+
+    marginTop: 30,
+  },
+
+  recommendContainer:
+  {
+    width: '100%',
+    marginVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: 120,
+    borderRadius: 10,
+    gap: 10,
+  },
+
+  descriptionContainer:
+  {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  descriptionText:
+  {
+    fontSize: 16,
+    fontWeight: 400,
+  },
+
+  recommendItems:
+  {
+    width: 120,
+    height: 100,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexShrink: 0,
+    marginRight: 10,
+  },
+
+  imageRec:
+  {
+    width: 70,
+    height: 70,
+  }
 });
---
+++ payment.js
@@ -0,0 +1,53 @@
+import React, { useRef } from 'react';
+import { Paystack, paystackProps } from 'react-native-paystack-webview';
+import { View, TouchableOpacity, Text } from 'react-native';
+
+function Pay({onClose}) {
+  // Ref to interact with Paystack WebView component
+  const paystackWebViewRef = useRef(paystackProps.PayStackRef);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {/* Paystack Webview for handling payment */}
+      <Paystack
+        paystackKey="pk_test_97ccd94767aae209e33c425d6e12a0b6357464a6" 
+        billingEmail="okpoco15@gmail.com"
+        currency='ZAR' 
+        amount={'25000.00'}
+        reference={'ORDER_1234567890'}
+        lastName='Poco'
+        firstName='Oscar'
+
+        onCancel={(e) => {
+          // Handle the payment cancellation
+          console.log('Payment cancelled', e);
+        }}
+        onSuccess={(res) => {
+          // Handle successful payment response
+          console.log('Payment successful', res);
+          alert(`Payment Successful! Payment ID: ${res.reference}`);
+        }}
+        ref={paystackWebViewRef}
+      />
+
+      {/* Button to trigger payment transaction */}
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#007bff',
+          padding: 10,
+          borderRadius: 5,
+          marginTop: 20,
+        }}
+        onPress={() => 
+            {
+                paystackWebViewRef.current?.startTransaction()
+                onClose()
+            }} 
+      >
+        <Text style={{ color: '#fff', fontSize: 18 }}>Pay Now</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default Pay;
